1844fdb2da0f160b6cceb736145a3ca54311717f
edwardfclark <edwardfclark@gmail.com>
2018-10-13 11:02:58 +0100

    Reorganized Document Flow
    - Took advantage of JavaScript's hoisting property to reorganize the
    document's flow to make it more readable. The flow is now Constants >
    Functions > D3 Appends.
    - Took much of the math out of the colorPicker() function. It was
    performing a lot of unnecessary operations. Given that this function is
    called for every single datapoint, this resulted in a much better load
    time.

--- script.js
+++ script.js
@@ -1,51 +1,14 @@
-//Color interpolation functions.
-
-//interpolate() returns a color value midway between the two provided color values. The degree of change depends on the factor.
-//This function is called inside the getGradient function.
-function interpolate(color1, color2, factor) {
-    if (arguments.length < 3) {
-        factor = 0.5;
-    }
-
-    let result = color1.slice();
-    for (let i = 0; i < 3; i++) {
-        result[i] = Math.round(result[i] + factor * (color2[i] - color1[i]));
-    }
-    return result;
-}
-
-//getGradient() takes three args - c1 and c2 are colors in rgb() format, and steps is the number of intermediary colors desired.
-//The function returns a 2D array. Each value in the array is another array of three numbers, designed to fit into rgb() values.
-function getGradient(c1, c2, steps) {
-    let stepFactor = 1 / (steps - 1), interpolatedArray = [];
-    c1 = c1.match(/\d+/g).map(Number);
-    c2 = c2.match(/\d+/g).map(Number);
-
-    for (let i = 0; i < steps; i++) {
-        interpolatedArray.push(interpolate(c1, c2, stepFactor * i));
-    }
-    return interpolatedArray;
-}
-
-//twoStepGradient() takes four args - firstColor, midColor, lastColor, and steps.
-//It calls twoStepGradient twice and returns a gradient that interpolates from firstColor to midColor, and then from midColor to lastColor.
-//In this way, we can go from blue to yellow to red, skipping intermediary colors that don't have semantic meaning with regard to temperature.
-function twoStepGradient(firstColor, midColor, lastColor, steps) {
-
-    let arr1 = getGradient(firstColor, midColor, steps);
-    let arr2 = getGradient(midColor, lastColor, steps);
-    let result = [...arr1, ...arr2];
-    return result;
-}
-
-//SVG creation with D3 library.
 document.addEventListener("DOMContentLoaded", function() {
-
+    
     let request = new XMLHttpRequest();
     request.open("GET", "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json", true);
     request.send();
-
     request.onload = function() {
+        
+
+        /* CONSTANTS */
+
+        //dataset holds the entirety of the JSON object for use in the program.
         const dataset = JSON.parse(request.responseText);
         const months = {1:"January", 2:"February", 3:"March", 4:"April", 5:"May", 6:"June", 7:"July", 8:"August", 9:"September", 10:"October", 11:"November", 12: "December"};
         
@@ -55,18 +18,46 @@ document.addEventListener("DOMContentLoaded", function() {
         const red = "rgb(255, 0, 0)";
         const gradientSteps = 100;
         const colors = twoStepGradient(blue, yellow, red, gradientSteps).map((d) => `rgb(${d.join(", ")})`);
-
-        //The pickColor() function takes a datapoint and calculates the color based on that input. It uses the d3.scaleLinear() functionality to map the input to an array value.
+        
+        //maxVariance, minVariance, and colorScale are used in the pickColor() function. maxVariance and minVariance are also used to define the legend scale.
+        const maxVariance = d3.max(dataset.monthlyVariance, (d) => d.variance);
+        const minVariance = d3.min(dataset.monthlyVariance, (d) => d.variance);
+        const colorScale = d3.scaleLinear().domain([minVariance, maxVariance]).range([0, colors.length-1]);
+        
+        //This is the div used for the tooltip. It's referenced inside the onmouseover statement in the rects section later in the program.
+        const div = d3.select("body").append("div").attr("id", "tooltip").style("opacity", 0);
+        
+        //Constants for defining the structure of the SVG.
+        const width = 1500;
+        const height = 750;
+        const padding = {top: 25, right: 75, bottom: 75, left: 75};
+        
+        //Constants for defining the structure of the legend.
+        const legendRectHeight = 10;
+        const legendY = height - 35;
+        const legendRectWidth = 3;
+        const legendWidth = (colors.length - 1) * legendRectWidth;
+        const baseTemp = dataset.baseTemperature;
+        const legendScale = d3.scaleLinear().domain([minVariance, maxVariance]).range([padding.left, padding.left+legendWidth]);
+        const legendAxis = d3.axisBottom(legendScale).tickFormat((d) => {
+            return d + baseTemp + "C";
+        });
+        
+        //Constant defining the svg.
+        const svg = d3.select("body").append("svg").attr("width", width).attr("height", height);
+        
+        //Constants for defining the scales and the axes.
+        const yearMin = d3.min(dataset.monthlyVariance, (d) => d["year"]);
+        const yearMax = d3.max(dataset.monthlyVariance, (d) => d["year"]);
+        const yScale = d3.scaleLinear().domain([12.5, 0.5]).range([height-padding.bottom, padding.top]);
+        const xScale = d3.scaleLinear().domain([yearMin, yearMax]).range([padding.left, width-padding.right]);
+        const yAxis = d3.axisLeft(yScale).tickFormat((d) => months[d]);
+        const xAxis = d3.axisBottom(xScale).ticks(26).tickFormat(d3.format("d"));
+        
         function pickColor(datapoint) {
-            
-            const max = d3.max(dataset.monthlyVariance, (d) => d.variance);
-            const min = d3.min(dataset.monthlyVariance, (d) => d.variance);
-            const colorScale = d3.scaleLinear().domain([min, max]).range([0, colors.length-1]);
-
             return colors[Math.round(colorScale(datapoint.variance))];
-            
         }
-
+        
         function generateTooltip(d) {
             let html = "<p>";
             html += d.year + " - " + months[d.month] + "<br />";
@@ -75,43 +66,50 @@ document.addEventListener("DOMContentLoaded", function() {
             html += "</p>";
             return html;
         }
-
-        const div = d3.select("body").append("div").attr("id", "tooltip").style("opacity", 0);
-    
-        //Constants for defining the structure of the SVG.
-        const width = 1500;
-        const height = 750;
-        const padding = {top: 25, right: 75, bottom: 75, left: 75};
-
-        //Constants for defining the structure of the legend.
-        const legendRectHeight = 10;
-        const legendY = height - 35;
-        const legendRectWidth = 3;
-        const legendWidth = (colors.length - 1) * legendRectWidth;
-        const baseTemp = dataset.baseTemperature;
-        const minVariance = d3.min(dataset.monthlyVariance, (d) => d.variance);
-        const maxVariance = d3.max(dataset.monthlyVariance, (d) => d.variance);
-        const legendScale = d3.scaleLinear().domain([minVariance, maxVariance]).range([padding.left, padding.left+legendWidth]);
-        const legendAxis = d3.axisBottom(legendScale).tickFormat((d) => {
-            return d + baseTemp + "C";
-        });
-        console.log(baseTemp);
-
         
-        const svg = d3.select("body").append("svg").attr("width", width).attr("height", height);
 
-        //Constants for defining the scales and the axes.
-        const yearMin = d3.min(dataset.monthlyVariance, (d) => d["year"]);
-        const yearMax = d3.max(dataset.monthlyVariance, (d) => d["year"]);
-        const yScale = d3.scaleLinear().domain([12.5, 0.5]).range([height-padding.bottom, padding.top]);
-        const xScale = d3.scaleLinear().domain([yearMin, yearMax]).range([padding.left, width-padding.right]);
+        /* FUNCTIONS */
         
-        const yAxis = d3.axisLeft(yScale).tickFormat((d) => {
-            return months[d];
-        });
-        const xAxis = d3.axisBottom(xScale).ticks(26).tickFormat(d3.format("d"));
+        //interpolate() returns a color value midway between the two provided color values. The degree of change depends on the factor.
+        //This function is called inside the getGradient function.
+        function interpolate(color1, color2, factor) {
+            if (arguments.length < 3) {
+                factor = 0.5;
+            }
+        
+            let result = color1.slice();
+            for (let i = 0; i < 3; i++) {
+                result[i] = Math.round(result[i] + factor * (color2[i] - color1[i]));
+            }
+            return result;
+        }
+        
+        //getGradient() takes three args - c1 and c2 are colors in rgb() format, and steps is the number of intermediary colors desired.
+        //The function returns a 2D array. Each value in the array is another array of three numbers, designed to fit into rgb() values.
+        function getGradient(c1, c2, steps) {
+            let stepFactor = 1 / (steps - 1), interpolatedArray = [];
+            c1 = c1.match(/\d+/g).map(Number);
+            c2 = c2.match(/\d+/g).map(Number);
+        
+            for (let i = 0; i < steps; i++) {
+                interpolatedArray.push(interpolate(c1, c2, stepFactor * i));
+            }
+            return interpolatedArray;
+        }
+        
+        //twoStepGradient() takes four args - firstColor, midColor, lastColor, and steps.
+        //It calls twoStepGradient twice and returns a gradient that interpolates from firstColor to midColor, and then from midColor to lastColor.
+        //In this way, we can go from blue to yellow to red, skipping intermediary colors that don't have semantic meaning with regard to temperature.
+        function twoStepGradient(firstColor, midColor, lastColor, steps) {
+            let arr1 = getGradient(firstColor, midColor, steps);
+            let arr2 = getGradient(midColor, lastColor, steps);
+            let result = [...arr1, ...arr2];
+            return result;
+        }
+        
 
-
+        /* D3 APPENDS */
+        
         //Rect elements indicating each datapoint.
         svg.selectAll("rect")
         .data(dataset.monthlyVariance)
@@ -165,10 +163,10 @@ document.addEventListener("DOMContentLoaded", function() {
         .attr("id", "legend-axis")
         .call(legendAxis);
 
+        //Text labels.
+
         //This is for testing, delete later.
-        document.getElementById("test").innerHTML = d3.max(dataset.monthlyVariance, (d) => d.variance) + dataset.baseTemperature;
+        //document.getElementById("test").innerHTML = d3.max(dataset.monthlyVariance, (d) => d.variance) + dataset.baseTemperature;
     }
 
-    //console.log(getGradient("rgb(26, 67, 204)", "rgb(204, 31, 26)", 50));
-    
 });
